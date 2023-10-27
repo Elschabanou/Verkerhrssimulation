1426903ced77af974b9dee3ce96c3e654469be40
Arne Sponer <arnesponer@gmail.com>
2023-10-27 14:39:36 +0200

    b

--- Verkehrssimulation/src/verkehrssimulation/Car.java
+++ Verkehrssimulation/src/verkehrssimulation/Car.java
@@ -3,5 +3,28 @@ package verkehrssimulation;
 
 
 public class Car {
-    
+
+    public int id;
+    double velocity = 0;
+    double maxAcc;
+    double maxDcc;
+    int mass;
+    String colour;
+    Section section;
+    double relPos = 0;
+
+    public Car(int id, String colour, double maxAcc, double maxDcc, int mass, Section section) {
+        id = id;
+        colour = colour;
+        maxAcc = maxAcc;
+        maxDcc = maxDcc;
+        mass = mass;
+        section = section;
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
 }
