a7120883644146cd29cf27e50cf3385204f05e6a
Elschabanou <felix@schabana.de>
2023-10-30 14:53:34 +0100

    komische autos

--- Verkehrssimulation/src/verkehrssimulation/GUI.java
+++ Verkehrssimulation/src/verkehrssimulation/GUI.java
@@ -27,9 +27,11 @@ public class GUI {
 
     
     private static final int ANIMATION_DELAY = 10; // Zeitverzögerung zwischen Frames in Millisekunden
-
+    public ArrayList<Car> cars = new ArrayList<>();
     private int imageX = 0; // Anfangsposition des Bildes auf der x-Achse
     private BufferedImage image;
+    
+    
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
@@ -39,14 +41,17 @@ public class GUI {
 
     public void updateGUI(ArrayList<Car> cars) {
         // Hier können Sie Ihre GUI basierend auf der Liste der Autos aktualisieren
+           
+        
     }
 
     public static void createAndShowGUI() {
         GUI gui = new GUI();
-
+        
+   
         JFrame frame = new JFrame("Verkehrssimulation");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
+        frame.setSize(800, 400);
 
         JPanel drawingPanel = new JPanel() {
             @Override
@@ -55,6 +60,8 @@ public class GUI {
                 
                 int panelWidth = getWidth();
                 int panelHeight = getHeight();
+                int pos_start = 0;
+                
 
                 // Set the background color to blue
                 g.setColor(Color.BLUE);
@@ -62,8 +69,18 @@ public class GUI {
 
                 // Zeichnen des Bildes an seiner aktuellen Position (imageX)
                 g.setColor(new Color(105, 109, 110));
-                g.fillRect(0, panelWidth / 2 , 500, 150);
-                g.drawImage(gui.image, gui.imageX, panelHeight / 2 , this);
+                g.fillRect(0, panelHeight / 2 - 25, 500, 150);
+                
+                for (Car car: gui.cars){
+                    /*try{*/
+                        /*BufferedImage image = ImageIO.read(GUI.class.getResource("images/Taycan_Topview_white.png"));
+                        g.drawImage(image, pos_start, panelHeight /2, this);*/
+                        System.out.print("halo");
+                        
+                    /*} catch (IOException e){
+                        e.printStackTrace();
+                    }*/
+                }
             }
         };
 
@@ -86,6 +103,7 @@ public class GUI {
                     // Wenn das Bild das Panel verlässt, von links nach rechts, zurücksetzen
                     gui.imageX = -gui.image.getWidth();
                 }*/
+                gui.updateGUI(gui.cars);
                 drawingPanel.repaint(); // Das Panel neu zeichnen, um die Änderungen anzuzeigen
             }
         });
