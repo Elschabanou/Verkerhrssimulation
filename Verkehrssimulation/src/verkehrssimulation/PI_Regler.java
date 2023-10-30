01e094f5414d1223972dbc74279ff21c7591bbe4
Arne Sponer <arnesponer@gmail.com>
2023-10-30 13:36:39 +0100

    neue PI_Regler Klasse zur Reglung der BEschleunigung

---
+++ Verkehrssimulation/src/verkehrssimulation/PI_Regler.java
@@ -0,0 +1,18 @@
+
+package verkehrssimulation;
+
+/**
+ *
+ * @author Arne
+ */
+
+public class PI_Regler {
+
+    public PI_Regler(){
+        
+    }
+
+    public static void main(String[] args){
+
+    }
+}
