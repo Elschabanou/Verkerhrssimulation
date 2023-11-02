a2aefab7be246324f68c3dbf49246d74d234a892
Elschabanou <felix@schabana.de>
2023-11-02 10:20:09 +0100

    available colors comment

--- Verkehrssimulation/src/verkehrssimulation/Verkehrssimulation.java
+++ Verkehrssimulation/src/verkehrssimulation/Verkehrssimulation.java
@@ -50,7 +50,7 @@ public class Verkehrssimulation {
     
     private void makeCars(){
         cars.add(new Car("Taycan 4s","white",150000,-150000, sec.get(0),this));
-        cars.add(new Car("Taycan s","blue",91000,-100000, sec.get(0),this));
+        cars.add(new Car("Taycan s","grey",91000,-100000, sec.get(0),this));
         cars.add(new Car("Taycan GTS","green",91000,-100000, sec.get(0),this));
         cars.add(new Car("Taycan","beige",91000,-100000, sec.get(0),this));
         cars.add(new Car("Taycan 4s","white",91000,-120000, sec.get(0),this));
